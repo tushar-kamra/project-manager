cfd457ccd44a8964b9ca1f2f0bbde45f4f82f4e5
tushar-kamra <tushar.kamra25@gmail.com>
2025-04-12 18:53:18 +0530

    console removed

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,6 @@ function App() {
         });
     };
 
-    console.log(projectsState);
-
     let content;
     if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} />;
